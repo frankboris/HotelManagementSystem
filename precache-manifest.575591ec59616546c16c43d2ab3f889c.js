3c5a0790fdc542fc5773d36a3384d675e90671e8
FrankBoris <frankborisdakleu@gmail.com>
2020-08-20 09:24:45 +0100

    Updates

---
+++ precache-manifest.575591ec59616546c16c43d2ab3f889c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50f3b3287fb90307715fd60ae43ae84",
+    "url": "/HotelManagementSystem/index.html"
+  },
+  {
+    "revision": "eb8b4062bbef31ade6b4",
+    "url": "/HotelManagementSystem/static/css/main.7247d5a9.chunk.css"
+  },
+  {
+    "revision": "05173bc3d8083dba95e2",
+    "url": "/HotelManagementSystem/static/js/2.19a5c861.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/HotelManagementSystem/static/js/2.19a5c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8b4062bbef31ade6b4",
+    "url": "/HotelManagementSystem/static/js/main.377d048a.chunk.js"
+  },
+  {
+    "revision": "516fec89c587f94afacf",
+    "url": "/HotelManagementSystem/static/js/runtime-main.e4ab8d20.js"
+  },
+  {
+    "revision": "2b75f26156210fc0b82522467ad1bd5a",
+    "url": "/HotelManagementSystem/static/media/agency_fb.2b75f261.ttf"
+  },
+  {
+    "revision": "ad85068a12398656e20181972f0089be",
+    "url": "/HotelManagementSystem/static/media/app_bg.ad85068a.png"
+  },
+  {
+    "revision": "14fc9896583da6d2f16d4bc20e58c2c6",
+    "url": "/HotelManagementSystem/static/media/blue_bg.14fc9896.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/HotelManagementSystem/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/HotelManagementSystem/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/HotelManagementSystem/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/HotelManagementSystem/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/HotelManagementSystem/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "eb54846e51f7af8d953eaac51313783f",
+    "url": "/HotelManagementSystem/static/media/logo.eb54846e.png"
+  },
+  {
+    "revision": "76e8acdb7f9dae31e7d1121932799e2a",
+    "url": "/HotelManagementSystem/static/media/logo_mb3.76e8acdb.png"
+  }
+]);
